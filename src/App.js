f034fd98747d1c5fff1ae2cf25bec9a220185e8c
Jason Shih <jason_shih@hotmail.com>
2022-12-15 18:29:05 -0800

    Add AppBar and Drawer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import CommandsContextProvider from './contexts/CommandsContext';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import ClientArea from './components/ClientArea';
+import SwipeableTemporaryDrawer from './components/SwipeableTemporaryDrawer';
+import AppBar from './components/AppBar'
 
 function App() {
   const theme = createTheme()
@@ -10,7 +12,9 @@ function App() {
     <div className="App">
       <CommandsContextProvider>
         <ThemeProvider theme={theme}>
-          <ClientArea />
+          <AppBar />
+          {/* <SwipeableTemporaryDrawer /> */}
+          {/* <ClientArea /> */}
         </ThemeProvider>
       </CommandsContextProvider>
     </div>
